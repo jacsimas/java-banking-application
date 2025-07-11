dd18bef945a9d91e8ae6e0e48e988d881662d43c
jacsimas <jacsimas@gmail.com>
2025-07-11 18:12:43 +0100

    dep. injection implementation addon - env. variables

--- src/main/java/org/example/DatabaseAPI.java
+++ src/main/java/org/example/DatabaseAPI.java
@@ -14,19 +14,10 @@ PreparedStatement connection;
 
     public DatabaseAPI() throws SQLException {
     }
-  /*  final String url = System.getenv("DB_URL");      // put into properties file
-    final String username = System.getenv("DB_USER");
-    final String password = System.getenv("DB_PASSWORD");
-*/
-  /*  private final String url = "jdbc:postgresql://localhost:5432/findb";
-    private final String username = "simon";
-    private final String password = "password";
-*/
+
     @Override
     public Connection connect() throws SQLException {
-    return java.sql.DriverManager.getConnection("jdbc:postgresql://localhost:5432/findb", "simon", "password");
+    return java.sql.DriverManager.getConnection(System.getenv("DB_URL"),System.getenv("DB_USER"), System.getenv("DB_PASSWORD"));
     }
-
-
 }
 

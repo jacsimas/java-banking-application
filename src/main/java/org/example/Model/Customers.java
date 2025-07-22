8a65889f0b5c60bd9d7131677df09bb4a36efac1
jacsimas <jacsimas@gmail.com>
2025-07-22 18:53:19 +0100

    adding springboot features

--- src/main/java/org/example/Model/Customers.java
+++ src/main/java/org/example/Model/Customers.java
@@ -1,10 +1,6 @@
 package org.example.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 
 @Entity

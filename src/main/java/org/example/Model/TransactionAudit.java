31f0c7fd9d21dbf020e3cf96dce8289d082ecac5
jacsimas <jacsimas@gmail.com>
2025-08-11 20:38:46 +0100

    I added camelCase to entities

--- src/main/java/org/example/Model/TransactionAudit.java
+++ src/main/java/org/example/Model/TransactionAudit.java
@@ -8,46 +8,38 @@ public class TransactionAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private int sender_id;
-    private String sender_nickname;
-    private int amount_sent;
-    private int receiver_id;
-    private String receiver_nickname;
+    private int senderId;
+    private String senderNickname;
+    private int amountSent;
+    private int receiverId;
+    private String receiverNickname;
     private String time;
 
     public TransactionAudit() {}
 
-    public TransactionAudit(int sender_id, String sender_nickname, int amount_sent, int receiver_id, String receiver_nickname, String time){
-        this.sender_id =sender_id;
-        this.sender_nickname = sender_nickname;
-        this.amount_sent=amount_sent;
-        this.receiver_id = receiver_id;
-        this.receiver_nickname=receiver_nickname;
-        this.time=time;
-    }
 
     public long getId() {
         return id;
     }
 
     public int getSender_id() {
-        return sender_id;
+        return senderId;
     }
 
     public String getSender_nickname() {
-        return sender_nickname;
+        return senderNickname;
     }
 
     public int getAmount_sent() {
-        return amount_sent;
+        return amountSent;
     }
 
     public int getReceiver_id() {
-        return receiver_id;
+        return receiverId;
     }
 
     public String getReceiver_nickname() {
-        return receiver_nickname;
+        return receiverNickname;
     }
 
     public String getTime() {

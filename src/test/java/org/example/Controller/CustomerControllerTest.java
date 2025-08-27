f30e2c285b1b0ede967e16f3226c3e16551bff65
jacsimas <jacsimas@gmail.com>
2025-08-27 19:31:52 +0200

    Added CustomerController tests, one for POST does not work, so far

--- src/test/java/org/example/Controller/CustomerControllerTest.java
+++ src/test/java/org/example/Controller/CustomerControllerTest.java
@@ -2,28 +2,57 @@ package org.example.Controller;
 
 import org.example.Model.Customers;
 import org.example.Services.CustomerEntityService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.json.JsonAssert;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/customers")
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CustomerControllerTest {
 
-    CustomerEntityService customerEntityService;
+    @Autowired
+    private MockMvc mockMvc;
 
-    public CustomerControllerTest(CustomerEntityService customerEntityService) {
-        this.customerEntityService = customerEntityService;
+    @Test
+     void shouldGetCustomerByIdTest() throws Exception {
+
+        this.mockMvc.perform(get("/customers/other-id?id=5")).andDo(print()).andExpect(status().isOk());
     }
 
+    //TODO: THIS TEST DOWN BELOW FAILED, I WILL HAVE TO UPDATE IT
+    @Test
+    void  shouldRegisterCustomerTest() throws Exception {
+        this.mockMvc.perform(post("/customers/register")).andDo(print()).andExpect(status().isOk());
 
-    @GetMapping
-    public List<Customers> getAllTest() {
-        return customerEntityService.allCustomers();
     }
 
+    @Test
+    void shouldGetAll()  throws Exception {
 
+        this.mockMvc.perform(get("/customers")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldSend ()  throws Exception {
+
+        this.mockMvc.perform(patch("/customers/add-money/1/100")).andDo(print()).andExpect(status().isOk());
+    }
 
 }
+//   .andExpect(content().string(containsString("Hello, World")));

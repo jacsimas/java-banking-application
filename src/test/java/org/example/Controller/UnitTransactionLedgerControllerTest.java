e0aabf56944e3cb6757747a96913bc9f3c781a77
jacsimas <jacsimas@gmail.com>
2025-08-29 20:48:12 +0200

    I added unit test for one transaction ledger controller method that receives GET

--- src/test/java/org/example/Controller/UnitTransactionLedgerControllerTest.java
+++ src/test/java/org/example/Controller/UnitTransactionLedgerControllerTest.java
@@ -1,11 +1,11 @@
 package org.example.Controller;
 
-import jakarta.transaction.Transactional;
+import org.example.Model.TransactionLedger;
+import org.example.Repositories.TransferAuditRepository;
 import org.example.Services.TransactionLedgerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,17 +15,26 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-public class TransactionLedgerControllerTest {
+@WebMvcTest(controllers = TransactionLedgerController.class)
+public class UnitTransactionLedgerControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
+    @MockitoBean
+    TransactionLedgerService transactionLedgerService;
+
+    @MockitoBean
+    TransferAuditRepository repository;
+
+//    @MockitoBean
+//    TransactionLedger transactionLedger;
+
+    //TODO: find out how this one works with POST, now its only for GET
     @org.junit.jupiter.api.Test
-    void shouldMakeTheTransfer() throws Exception {
-//        when(transactionLedgerService.updateTransactionLedger()).thenReturn("done");
+    void shouldMakeTheTransferMockedService() throws Exception {
+        when(transactionLedgerService.getTransactionAudit(1))
+                .thenReturn(new TransactionLedger());
         this.mockMvc.
                 perform(post("/payments/transaction")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -33,6 +42,5 @@ public class TransactionLedgerControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-
-
 }
+// transactionLedger:  1, "derry", 100, 2, "john"
--- src/test/java/org/example/Services/TransactionLedgerServiceTest.java
+++ src/test/java/org/example/Services/TransactionLedgerServiceTest.java
@@ -1,4 +1,29 @@
 package org.example.Services;
 
+import org.example.Model.TransactionLedger;
+import org.example.Repositories.TransferAuditRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
 public class TransactionLedgerServiceTest {
+
+    @Mock
+    TransferAuditRepository transferAuditRepository;
+    @InjectMocks
+    TransactionLedgerService transactionLedgerService;
+
+//    @Test
+//    public void updateTransactionLedgerTest(){
+//     //   {\"senderId\":1,\"senderNickname\":\"derry\",\"amountSent\":100,\"receiverId\":2,\"receiverNickname\":\"john\"}"
+//
+//        TransactionLedger payment = new TransactionLedger(1, "derry");
+//        assertThat().isEqual.
+//    }
+
 }

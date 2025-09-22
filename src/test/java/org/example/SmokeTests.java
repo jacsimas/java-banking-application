e90db32809daa2f8766b1dfe67de0cf0a13ad714
jacsimas <jacsimas@gmail.com>
2025-09-22 17:24:25 +0200

    added SmokeTests.java

---
+++ src/test/java/org/example/SmokeTests.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SmokeTests {
+
+    @Test
+    void it_works() {
+        assertTrue(true);
+    }
+}

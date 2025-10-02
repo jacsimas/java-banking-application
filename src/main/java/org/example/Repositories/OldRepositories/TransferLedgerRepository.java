559e7b77e5915ae209f9f3c1e889533dfeb5747c
jacsimas <jacsimas@gmail.com>
2025-10-02 11:27:26 +0200

    deleted old version service, repository, controller, model class files

--- src/main/java/org/example/Repositories/OldRepositories/TransferLedgerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.Repositories.OldRepositories;
-
-import org.example.Model.OldModels.TransactionLedger;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransferLedgerRepository extends CrudRepository<TransactionLedger, Long> {
-}
